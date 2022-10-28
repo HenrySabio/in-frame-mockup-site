57ce8e62900f1e4077c33fa924da77a00053dea3
Henry Sabio <henry@sabio.sh>
2022-10-28 14:05:45 -0400

    Nav switched to right side

--- js/script.js
+++ js/script.js
@@ -2,11 +2,11 @@ const toggleTag = document.querySelector('.toggle-nav');
 const mainTag = document.querySelector('main');
 
 toggleTag.addEventListener('click', function () {
+    toggleTag.classList.toggle('open');
     mainTag.classList.toggle('open');
-
     if (mainTag.classList.contains('open')) {
-        toggleTag.innerHTML = '<img src="img/close.svg" alt="close menu"> Close';
+        toggleTag.innerHTML = '<img src="img/close.svg" alt="close menu"><span>Close</span>';
     } else {
-        toggleTag.innerHTML = '<img src="img/menu.svg" alt="open menu"> Menu';
+        toggleTag.innerHTML = '<img src="img/menu.svg" alt="open menu"><span>Menu</span>';
     }
 });
